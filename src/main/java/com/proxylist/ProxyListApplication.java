9b43c7435411054066fae66064e4bf2f496f386c
hyeonjerry <kwonhyeonjae01@gmail.com>
2025-04-05 16:58:13 +0900

    Project Init

---
+++ src/main/java/com/proxylist/ProxyListApplication.java
@@ -0,0 +1,13 @@
+package com.proxylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyListApplication.class, args);
+	}
+
+}
